426878e997f6ba682a8a0f19cbc43de1ecbb346b
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2022-08-04 16:41:19 -0400

    Added dialog box to HuntActivity

--- app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/HuntActivity.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/HuntActivity.kt
@@ -2,14 +2,14 @@ package com.matthew.carvalhodagenais.coinhuntingbuddy
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.matthew.carvalhodagenais.coinhuntingbuddy.ui.theme.CoinHuntingBuddyTheme
@@ -30,6 +30,34 @@ class HuntActivity : ComponentActivity() {
                     )
                 }
                 Surface {
+
+                    val showAlertDialog = remember { mutableStateOf(false)}
+                    BackHandler {
+                        showAlertDialog.value = true
+                    }
+                    if(showAlertDialog.value){
+                        AlertDialog(
+                            onDismissRequest = { /* Do nothing */ },
+                            confirmButton = {
+                                TextButton(onClick = {
+                                    val intent = Intent(this, MainActivity::class.java)
+                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    startActivity(intent)
+                                }){
+                                    Text(text = "Yes")
+                                }
+                            },
+                            dismissButton = {
+                                TextButton(onClick = { showAlertDialog.value = false }) {
+                                    Text(text = "No")
+                                }
+                            },
+                            title = { Text(text = "Are you sure you want to stop this hunt?") },
+                            text = { Text(text = "This hunt and any finds will not be saved.") }
+                        )
+                    }
+
+
                     Column {
                         TopAppBar(
                             backgroundColor = Color.White,
