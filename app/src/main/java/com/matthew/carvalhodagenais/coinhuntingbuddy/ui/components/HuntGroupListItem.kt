07f59bcaed9eb6e3ae8586d8b4d7eabfccfb8d7f
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2023-02-19 19:09:57 -0500

    Make hunt list look a bit better

--- app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/components/HuntGroupListItem.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/components/HuntGroupListItem.kt
@@ -1,14 +1,14 @@
 package com.matthew.carvalhodagenais.coinhuntingbuddy.ui.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.matthew.carvalhodagenais.coinhuntingbuddy.data.entities.HuntGroup
 import com.matthew.carvalhodagenais.coinhuntingbuddy.utils.DateToStringConverter
 import com.matthew.carvalhodagenais.coinhuntingbuddy.viewmodels.MainActivityViewModel
@@ -29,7 +29,7 @@ fun HuntGroupListItem(
             val list = viewModel.getHuntType(huntGroup).await()
 
             if (list.size > 1) {
-                huntTypeString.value = "Multiple"
+                huntTypeString.value = "Multiple Coin Types"
             } else if (list.size == 1) {
                 huntTypeString.value = list[0].name
             } else {
@@ -41,17 +41,40 @@ fun HuntGroupListItem(
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(start = itemPadding, end = itemPadding)
             .clickable { onClick() }
+            .padding(top = 2.dp, bottom = 2.dp)
     ) {
-        Row {
-            Text(text = DateToStringConverter.getString(huntGroup.dateHunted))
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                start = itemPadding,
+                end = itemPadding,
+                bottom = 2.dp
+            )
+        ) {
+            Text(
+                text = DateToStringConverter.getString(huntGroup.dateHunted),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
-        Row {
-            Column {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                start = itemPadding,
+                end = itemPadding
+            )
+        ) {
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.Start
+            ) {
                 Text(text = huntTypeString.value)
             }
-            Column {
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.End
+            ) {
                 Text(text = huntGroup.regionId.toString())
             }
         }
--- app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/screens/HuntsScreen.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/screens/HuntsScreen.kt
@@ -2,10 +2,14 @@ package com.matthew.carvalhodagenais.coinhuntingbuddy.ui.screens
 
 import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -49,16 +53,23 @@ fun HuntsScreen(
         } else if (allHunts!!.isEmpty()) {
             NoItemsWarning(topText = "No hunts", bottomText = "Click \"+\" to start one!")
         } else if (allHunts!!.isNotEmpty()) {
-            Column {
-                allHunts!!.forEach {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                allHunts!!.forEachIndexed { index, it ->
                     HuntGroupListItem(
                         huntGroup = it,
                         viewModel = viewModel,
                         onClick = {
                             // TODO: Go to a description page
-                            Log.e("CLICK", "Row clicked! ID: ${it.id}")
                         }
                     )
+
+                    if (index != allHunts!!.lastIndex) {
+                        Divider(
+                            color = Color.LightGray,
+                            thickness = 1.dp,
+                            modifier = Modifier.padding(bottom = 2.dp, top = 2.dp, start = 2.dp, end = 2.dp)
+                        )
+                    }
                 }
             }
         }
