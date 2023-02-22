cdf668a476d9a1f05ca5367ab7429a74be2bbd96
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2023-02-21 19:56:20 -0500

    Minor code cleanup

--- app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/screens/DetailsScreen.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/screens/DetailsScreen.kt
@@ -1,6 +1,5 @@
 package com.matthew.carvalhodagenais.coinhuntingbuddy.ui.screens
 
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
