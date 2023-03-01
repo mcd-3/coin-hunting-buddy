ff07e953131c12042757a98ebf9e26725b09bd67
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2023-03-01 00:22:01 -0500

    Add colors to system bars and text fields

--- app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/theme/Theme.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/theme/Theme.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -51,6 +52,11 @@ fun CoinHuntingBuddyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(
+        color = if (darkTheme) Gray700 else Silver200
+    )
+
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
