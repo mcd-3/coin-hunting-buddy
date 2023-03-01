5a4614d5dc62010a8767bb9d0ac74199c2aefdd9
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2023-03-01 01:59:57 -0500

    Code cleanup

--- app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/components/SummaryFinds.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/components/SummaryFinds.kt
@@ -6,13 +6,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.ParagraphStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextIndent
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.matthew.carvalhodagenais.coinhuntingbuddy.data.entities.Find
