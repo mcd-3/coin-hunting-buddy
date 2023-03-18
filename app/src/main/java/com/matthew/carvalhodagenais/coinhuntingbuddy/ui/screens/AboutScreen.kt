a99fa9ce2785200729eac7e00487abff5fbc8297
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2023-03-18 18:53:45 -0400

    Create basic About screen

--- app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/screens/AboutScreen.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/screens/AboutScreen.kt
@@ -1,17 +1,25 @@
 package com.matthew.carvalhodagenais.coinhuntingbuddy.ui.screens
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.matthew.carvalhodagenais.coinhuntingbuddy.R
-import com.matthew.carvalhodagenais.coinhuntingbuddy.ui.components.NavDrawer
 import com.matthew.carvalhodagenais.coinhuntingbuddy.ui.components.AppBar
+import com.matthew.carvalhodagenais.coinhuntingbuddy.ui.components.NavDrawer
+import com.matthew.carvalhodagenais.coinhuntingbuddy.ui.theme.secondaryText
+
 
 private const val ABOUT_INDEX = 2
 
@@ -37,9 +45,101 @@ fun AboutScreen(navController: NavController) {
         drawerElevation = 12.dp,
         drawerScrimColor = Color.Black.copy(0.3f)
     ) {
-        Box(modifier = Modifier
-            .background(Color.White)
-            .fillMaxSize()) {
-            Text(text = "about")}
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Column(modifier = Modifier.weight(1f)) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text("Coin Roll Hunter Buddy", fontSize = 20.sp)
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 8.dp, bottom = 8.dp),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            modifier = Modifier.align(alignment = Alignment.CenterVertically),
+                            painter = painterResource(id = R.drawable.ic_logo_gradient_128),
+                            contentDescription = "",
+                            contentScale = ContentScale.Fit
+                        )
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 8.dp, bottom = 8.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text("By: mcd-3", fontSize = 18.sp)
+                    }
+                }
+
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 16.dp, end = 16.dp)
+                ) {
+                    Column(modifier = Modifier.weight(1f)) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp, bottom = 8.dp),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                "This app is open source and licensed under MIT.",
+                                textAlign = TextAlign.Center
+                            )
+                        }
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp, bottom = 8.dp),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                "Click here to view the license.",
+                                textAlign = TextAlign.Center
+                            ) // This needs to be a link
+                        }
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp, bottom = 8.dp),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                "Need support? Send me an email!",
+                                textAlign = TextAlign.Center
+                            ) // Needs to be an email link
+                        }
+                    }
+
+                    Column(modifier = Modifier.weight(1f).fillMaxSize()) {
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            Text(
+                                text = stringResource(
+                                    id = R.string.version_label,
+                                    stringResource(id = R.string.version)
+                                ),
+                                fontStyle = FontStyle.Italic,
+                                color = MaterialTheme.colors.secondaryText,
+                                modifier = Modifier
+                                    .align(Alignment.BottomEnd)
+                                    .padding(bottom = 12.dp, end = 14.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
