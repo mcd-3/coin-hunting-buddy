2ed54e1e1f9420e7211fdb46e2896e7f7c7622c1
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2023-03-19 23:35:56 -0400

    Add delete pop up

--- app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/coinhuntingbuddy/ui/screens/SettingsScreen.kt
@@ -1,19 +1,24 @@
 package com.matthew.carvalhodagenais.coinhuntingbuddy.ui.screens
 
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.matthew.carvalhodagenais.coinhuntingbuddy.R
 import com.matthew.carvalhodagenais.coinhuntingbuddy.ui.components.NavDrawer
 import com.matthew.carvalhodagenais.coinhuntingbuddy.ui.components.AppBar
+import com.matthew.carvalhodagenais.coinhuntingbuddy.ui.components.ConfirmCancelAlertDialog
 import com.matthew.carvalhodagenais.coinhuntingbuddy.ui.components.SettingsButton
 
 private const val SETTINGS_INDEX = 3
@@ -21,6 +26,9 @@ private const val SETTINGS_INDEX = 3
 @Composable
 fun SettingsScreen(navController: NavController) {
     val scaffoldState = rememberScaffoldState()
+    val context = LocalContext.current
+
+    val deleteWarningState = remember { mutableStateOf(false) }
 
     // Back Handler to go back to the hunts screen
     BackHandler {
@@ -66,11 +74,30 @@ fun SettingsScreen(navController: NavController) {
                     topText = "Delete Data",
                     bottomText = "Deletes all hunts and finds from the app",
                     onClick = {
-                        Log.e("CLICKED", "Oh no!")
+                        deleteWarningState.value = true
                     },
                     isDanger = true
                 )
             }
         }
+    }
+    
+    if (deleteWarningState.value) {
+        ConfirmCancelAlertDialog(
+            title = "Delete Data",
+            body = "Are you sure you want to delete all hunts and finds from the app?\n\nTHIS ACTION IS PERMANENT!!",
+            confirmLabel = "DELETE",
+            cancelLabel = "CANCEL",
+            toggledState = deleteWarningState,
+            onConfirm = {
+                deleteWarningState.value = false
+                Toast.makeText(
+                    context,
+                    "App Data Deleted",
+                    Toast.LENGTH_LONG
+                ).show()
+            },
+            onCancel = { deleteWarningState.value = false }
+        )
     }
 }
